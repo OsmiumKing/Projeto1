4aea5a1c30a1731c735bbe8f031eb0c81e49007e
Muskatif <63464521+Muskatif@users.noreply.github.com>
2022-11-21 17:11:46 +0000

    Add files via upload

--- Codi_pizzer.ino
+++ Codi_pizzer.ino
@@ -1,44 +1,59 @@
-int speakerPin=10 ;
-int length=5;
-char notes[]="cf";
-int beats[]={1,1,1,1};
-int tempo=300;
+int speakerPin = 10;
+int ldrpin = A0;
+int length = 5;
+char notes[] = "cf";
+int beats[] = { 1, 1, 1, 1 };
+int tempo = 300;
+int luz;
 
-void playTone(int tone, int duration){
-  for (long i=0; i< duration*1000L; i+=tone*2){
-    digitalWrite(speakerPin,HIGH);
+void playTone(int tone, int duration) {
+  for (long i = 0; i < duration * 1000L; i += tone * 2) {
+    digitalWrite(speakerPin, HIGH);
     delayMicroseconds(tone);
-    digitalWrite(speakerPin,LOW);
+    digitalWrite(speakerPin, LOW);
     delayMicroseconds(tone);
   }
 }
 
-void playNote(char note,int duration){
-  char names[]={'c','d','e','f','g','a','b','C'};
-  int tones[]={1915,1700,1519,1432,1275,1136,1024,956};
-  
-  for(int i=0;i<8;i++){
-    if(names[i]==note)
-      playTone(tones[i],duration);
+void playNote(char note, int duration) {
+  char names[] = { 'c', 'd', 'e', 'f', 'g', 'a', 'b', 'C' };
+  int tones[] = { 1915, 1700, 1519, 1432, 1275, 1136, 1024, 956 };
+
+  for (int i = 0; i < 8; i++) {
+    if (names[i] == note)
+      playTone(tones[i], duration);
   }
 }
 
-void setup()
-{
-  pinMode(A0,INPUT);
+void setup() {
+  pinMode(ldrpin, INPUT);
   pinMode(speakerPin, OUTPUT);
-  SerialBegin
+  Serial.begin(9600);
 }
 
-  
-void loop()
-{
-  for(int i=0;i<length;i++){
-    if(notes[i]==' ')
-      delay(beats[i]*tempo);
-    else
-    playNote(notes[i],beats[i]*tempo);
-    
-    delay(tempo/2);
+
+void loop() {
+  int tempo = 0;
+  int t;
+  luz = analogRead(ldrpin);
+
+  tempo = millis();
+  do {
+    if (luz != (analogRead(ldrpin) + 10)) {
+      loop();
+    }
+    t = millis() - tempo;
+  } while (t < 120000);
+  SL();
 }
+
+void SL() {
+  for (int i = 0; i < length; i++) {
+    if (notes[i] == ' ')
+      delay(beats[i] * tempo);
+    else
+      playNote(notes[i], beats[i] * tempo);
+
+    delay(tempo / 2);
+  }
 }
